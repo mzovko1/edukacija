284d0240f86b340f24c6a5d8a578c36e3eb012b9
Matea Zovko <73528711+mzovko1@users.noreply.github.com>
2024-02-19 11:45:08 +0100

    Add files via upload

---
+++ Zadatak1/Program.cs
@@ -0,0 +1,22 @@
+﻿Console.WriteLine($"Unesite broj učenika:");
+int brojUcenika=Convert.ToInt32(Console.ReadLine()); //unos broja učenika
+
+
+for (int i=0;i<brojUcenika;i++) 
+{
+    Console.WriteLine($"Unesite ocjenu za učenika {i + 1}:");
+    int ocjena = Convert.ToInt32(Console.ReadLine()); //unos ocjene iz ispita
+                                                 
+    if(ocjena>1 && ocjena<=5)
+    {
+        Console.WriteLine($"Ucenik {i+1} prošao je ispit! Ocjena je {ocjena}");
+    }
+    else if(ocjena==1)
+    {
+        Console.WriteLine($"Učenik {i+1} pao je ispit. Ocjena je {ocjena}");
+    }
+    else
+    {
+        Console.WriteLine($"Neispravno unesena ocjena");
+    }
+}
